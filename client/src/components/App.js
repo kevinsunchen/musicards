ec0e5f0fcbf9e9e30a2bb9382b39d875450724bb
kevinsunchen <kschen0520@gmail.com>
2021-01-15 13:05:14 -0800

    testing for deployment

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -41,7 +41,7 @@ class App extends Component {
     console.log(`Logged in as ${res.profileObj.name}`);
     const userToken = res.tokenObj.id_token;
     post("/api/login", { token: userToken }).then((user) => {
-      this.setState({ userId: user._id }, () => navigate(`/profile/${this.state.userId}`));
+      this.setState({ userId: user._id });
       post("/api/initsocket", { socketid: socket.id });
     });
   };
