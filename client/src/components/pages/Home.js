2df62145277bd125c1b47008ffbba11030bfdb4d
amy-zhong <amyzhong4701@gmail.com>
2021-01-21 22:44:38 -0800

    css start for home page

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -4,6 +4,10 @@ import React, { Component } from "react";
 import "../../utilities.css";
 import "./Skeleton.css";
 import "./Home.css";
+import envelope from "../../public/envelope.png";
+import deck from "../../public/deck.png";
+import idcard from "../../public/idcard.png";
+
 
 class Home extends Component {
   constructor(props) {
@@ -16,30 +20,65 @@ class Home extends Component {
     document.title = "musicards!";
     // remember -- api calls go here!
   }
-
+//<h1>HOME</h1>
+//<h2>Home page. Includes icons linking to the public Requests feed, the user's Incoming cards page, the user's Deck page, and the user's Profile page.</h2>
+        
   render() {
     return (
       <>
-        <h1>HOME</h1>
-        <h2>Home page. Includes icons linking to the public Requests feed, the user's Incoming cards page, the user's Deck page, and the user's Profile page.</h2>
-        <Link to="/requests">
-          Requests <br />
-        </Link>
-        <Link to="/incoming">
-          Incoming <br />
-        </Link>
-        <Link to="/deck">
-          Deck <br />
-        </Link>
-        {this.props.loggedInUser ? (
-          <Link to={`/profile/${this.props.loggedInUser._id}`}>
-            Profile <br />
-          </Link>
-        ) : (
-          <Link to='/login'>
-            Login <br />
-          </Link>
-        )}
+        
+        <div className = "container"> 
+          <div className = "iconGroup"> 
+            <Link to="/requests">
+              requests <br />
+              <img src = {envelope} width = "500px" class = "icon"/> <br />
+            </Link>
+          </div>
+
+          <div className = "iconGroup"> 
+            {this.props.loggedInUser ? (
+              <Link to={`/profile/${this.props.loggedInUser._id}`}>
+                profile <br />
+                <img src = {idcard} width = "400px"  /> <br />
+              </Link>
+            ) : (
+              <Link to='/login'>
+                login <br />
+                <img src = {idcard} width = "400px" /> <br />
+              </Link>
+            )}
+            
+          </div>
+
+          <div class="break"></div>
+          <div class = "appname"> musicards! </div>
+          <div class="break"></div>
+          
+          <div className = "iconGroup"> 
+            <Link to="/incoming">
+              <img src = {deck} width = "400px"/> <br />
+              incoming <br />
+            </Link>
+          </div>
+
+          <div className = "iconGroup"> 
+            <Link to="/deck">
+              <img src = {deck} width = "400px" /> <br />
+              deck <br />
+            </Link>
+          </div>
+
+          
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
       </>
     );
   }
